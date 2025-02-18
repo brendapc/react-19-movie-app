22031f73672068dced4760b4282279e26eba26dc
Brenda Pereira <brendacamara@outlook.com>
2025-02-18 15:37:41 -0300

    feat: setting up home with assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
-
 const App = () => {
   return (
     <div>
       <div className="pattern"></div>
-      
+
       <div className="wrapper">
-        
+        <header>
+          <img src="./hero-img.png" alt="hero banner" />
+          <h1>
+            Find <span className="text-gradiant">Movies</span> You'll Enjoy
+          </h1>
+        </header>
       </div>
-      
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <p>Search</p>
     </div>
   );
 };
 
 export default App;
-
