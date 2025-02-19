14b17b6a27a445f473bd38ef42257747dca4f683
Brenda Pereira <brendacamara@outlook.com>
2025-02-19 19:17:33 -0300

    feat: create cards for movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -74,14 +75,7 @@ const App = () => {
           ) : (
             <ul>
               {movieList.map((movie: any) => (
-                <li key={movie.id}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <h3>{movie.title}</h3>
-                  <p>{movie.overview}</p>
-                </li>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
